2cd0c0a9cab98a751582312b2e70ad39441cbe75
bhoover59 <77543359+bhoover59@users.noreply.github.com>
2022-11-15 21:27:43 -0500

    Create char_to_time.R

---
+++ R/char_to_time.R
@@ -0,0 +1,17 @@
+# Author: Emily Reidy
+char_to_time <- function(TimeX){
+  formats = c("%m/%d/%Y %H:%M:%S", "%m/%d/%Y %I:%M:%S %p", "%Y-%m-%d %H:%M:%S", "%m/%d/%Y %H:%M", "%m/%d/%Y", "%Y-%m-%d", "%H:%M:%S")
+  #Add extra formats to character vector above
+  #formats tested in order, so go from most specific to least specific,
+  #as the first one that works will be used
+  for (i in 1:length(formats)){
+    test = as.POSIXct(TimeX, format = formats[i])
+    if (length(na.omit(test)) == length(test)) {
+      TimeX = as.POSIXct(TimeX, format = formats[i])
+      break
+    } else if (i == length(formats)){
+      print("Could not find adequate format, char_to_time function needs to be edited")
+    }
+  }
+  return(TimeX)
+}
