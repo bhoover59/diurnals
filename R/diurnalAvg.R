5b1b29b91065f2659ac2002157694120291dbbc2
bhoover59 <77543359+bhoover59@users.noreply.github.com>
2022-11-15 21:26:20 -0500

    Update and rename hello.R to diurnalAvg.R

---
+++ R/diurnalAvg.R
@@ -0,0 +1,34 @@
+DiurnalAvg <- function(df, TimeColumn){
+  # Convert time & create hourly bins
+  df[,TimeColumn] <- diurnals::char_to_time(df[,TimeColumn])
+  df$Hour <- substr(df$Time, 12, 13) # specific to this time format
+
+  # Mean of each bin
+  df_avg <- aggregate(df, by = list(df$Hour), FUN = mean, na.rm = TRUE)
+  df_avg <- df_avg[,!names(df_avg) %in% c("Hour")] # Delete Hour column filled with NA
+  colnames(df_avg)[1] <- "Hour" # rename Group.1 to Hour
+
+  # Standard deviation of each bin
+  df_avg_sd <- aggregate(df, by = list(df$Hour), FUN = sd, na.rm = TRUE)
+  df_avg_sd <- df_avg_sd[,!names(df_avg_sd) %in% c("Hour")] # Delete Hour column filled with NA
+  columns <- colnames(df_avg) # need to change names for merge
+  new_columns <- paste(columns, "_sd", sep = "")
+  colnames(df_avg_sd) <- new_columns
+  colnames(df_avg_sd)[1] <- "Hour"
+
+  # Count number of points in each bin
+  df_avg_count <- aggregate(df, by = list(df$Hour), FUN = length)
+  df_avg_count <- df_avg_count[,!names(df_avg_count) %in% c("Hour")] # Delete Hour column filled with NA
+  columns_count <- colnames(df_avg) # need to change names for merge
+  new_columns_count <- paste(columns_count, "_count", sep = "")
+  colnames(df_avg_count) <- new_columns_count
+  colnames(df_avg_count)[1] <- "Hour"
+
+  # Merge data frames
+  df_list <- list(df_avg, df_avg_sd, df_avg_count)
+  df_tot <- Reduce(function(x, y) merge(x, y, all = TRUE), df_list)
+  for (i in 0:9){
+    df_tot$Hour[i + 1] <- i # Change 00, 01, 02 etc to 0, 1, 2 etc
+  }
+  df_tot
+}
