aefeb8051245e6a578865ecdb853b14909ebac64
bhoover59 <77543359+bhoover59@users.noreply.github.com>
2023-02-16 10:28:28 -0500

    Update convert_time.R

--- R/convert_time.R
+++ R/convert_time.R
@@ -2,7 +2,7 @@ convert_time <- function(TimeX){
   if(inherits(TimeX, "POSIXct")){
     TimeX <- TimeX
   } else {
-    formats = c("%m/%d/%Y %H:%M:%S", "%m/%d/%Y %I:%M:%S %p", "%Y-%m-%d %H:%M:%S", "%m/%d/%Y %H:%M", "%m/%d/%Y", "%Y-%m-%d", "%H:%M:%S")
+    formats = c("%m/%d/%Y %H:%M:%S", "%m/%d/%Y %I:%M:%S %p", "%Y-%m-%d %H:%M:%S", "%m/%d/%Y %H:%M", "%m/%d/%y %H:%M", "%m/%d/%Y", "%Y-%m-%d", "%H:%M:%S")
     for (i in 1:length(formats)){
       test = as.POSIXct(TimeX, format = formats[i])
       if (length(na.omit(test)) == length(test)) {
